2d1694276575f47849c5da8f28fb18075574912e
deusyvan <deusyvan@gmail.com>
2018-06-11 22:29:20 -0300

    Carregando os js na página que lógico vai no final para não pesar a página

--- index.php
+++ index.php
@@ -10,5 +10,12 @@
 	</head>
 	<body>
 		<h1>Olá mundo!</h1>
+		
+		
+		
+		
+		
+		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
 	</body>
 </html>
