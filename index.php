8917eb4260a4c614df576d25ab5796f1cd132810
deusyvan <deusyvan@gmail.com>
2018-06-11 22:25:28 -0300

    Estrutura do projeto inserindo o css na index

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+		
+		<title>Projeto Bootstrap 4</title>
+	</head>
+	<body>
+		<h1>Olá mundo!</h1>
+	</body>
+</html>
