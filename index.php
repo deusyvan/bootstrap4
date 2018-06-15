b8b89d0ee47be88736eacbf12d90ca41534ce2dc
deusyvan <deusyvan@gmail.com>
2018-06-15 18:30:44 -0300

    Configurando o tamanho e distribuindo o resto do espaço

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		<div class="container">
     		<div class="row">
     			<div class="col" >...</div>
-    			<div class="col" >...</div>
+    			<div class="col-2" >...</div>
     			<div class="col" >...</div>
     			
     		</div>
