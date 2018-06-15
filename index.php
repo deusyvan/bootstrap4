89af71113be412320bd7e10a49eefc0f99139217
deusyvan <deusyvan@gmail.com>
2018-06-15 18:36:46 -0300

    Somente a 1 quebra para celular

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 		<div class="container">
     		<div class="row">
     			<div class="col-sm" >...</div>
-    			<div class="col-sm-2" >...</div>
-    			<div class="col-sm-2" >...</div>
+    			<div class="col" >...</div>
+    			<div class="col-2" >...</div>
     			
     		</div>
     		
