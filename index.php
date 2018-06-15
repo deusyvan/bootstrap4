8b4459258c557e3f3e30935ef226d15de1482070
deusyvan <deusyvan@gmail.com>
2018-06-15 18:33:49 -0300

    Coluna sem tamanho especifico

--- index.php
+++ index.php
@@ -17,10 +17,9 @@
 	<body>
 		<div class="container">
     		<div class="row">
-    			<div class="col-4" >...</div>
+    			<div class="col" >...</div>
     			<div class="col-2" >...</div>
     			<div class="col-2" >...</div>
-    			<div class="col-4" >...</div>
     			
     		</div>
     		<div class="row">
