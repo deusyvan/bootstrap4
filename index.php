a15d248f32708ec9765a2ca9acea76e22df592f3
deusyvan <deusyvan@gmail.com>
2018-06-15 21:58:50 -0300

    Começando estudo de tipografia

--- index.php
+++ index.php
@@ -8,51 +8,14 @@
 		
 		<title>Projeto Bootstrap 4</title>
 		<style type="text/css">
-		  .avatar {
-		      width: 50px;
-		      height: 50px;
-		  }
 		  
-		  .media {
-		      background-color: #EEEEEE;
-		      border-bottom: 1px solid #CCC;
-		  }
+		  
 		</style>
 	</head>
 	<body>
 		<div class="container">
-			<ul class="list-unstylied">
-        		<li class="media">
-        			<img alt="" src="assets/images/me.jpg" class="mr-3 avatar align-self-center">
-        			<div class="media-body">
-        				<h5>Algum tipo de titulo</h5>
-        				<p>Aqui vai um texto ilustrativo para mostrar o exemplo de um comentário,
-        				que servirá para vermos como vai ficar na tela do computador, para continuarmos
-        				o estudo de media que é um componente inspirado no facebook de comentários
-        				onde fica uma imagem e o conteúdo dele do lado da imagem como exemplo aqui
-        				mostrado neste comentário com a imagem logo em seguida.
-        			</div>
-        		</li>
-        		<li class="media">
-        			<img alt="" src="assets/images/me.jpg" class="mr-3 avatar align-self-end">
-        			<div class="media-body">
-        				<h5>Algum tipo de titulo</h5>
-        				<p>Aqui vai um texto ilustrativo para mostrar o exemplo de um comentário,
-        				que servirá para vermos como vai ficar na tela do computador, para continuarmos neste comentário com a imagem logo em seguida.
-        			</div>
-        		</li>
-        		<li class="media">
-        			<img alt="" src="assets/images/me.jpg" class="mr-3 avatar align-self-start"">
-        			<div class="media-body">
-        				<h5>Algum tipo de titulo</h5>
-        				<p>Aqui vai um texto ilustrativo para mostrar o exemplo de um comentário,
-        				qomo exemplo aqui mostrado neste comentário com a imagem logo em seguida.
-        			</div>
-        		</li>
-        		
-				
-			</ul>
-		
+			
+			
     		
 		</div>
 		
