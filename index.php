51a99dd7e395fd967d4d2745a67844fb0e294fd5
deusyvan <deusyvan@gmail.com>
2018-06-16 10:18:59 -0300

    Começando estudo de input group

--- index.php
+++ index.php
@@ -15,41 +15,6 @@
 	<body>
 		<div class="container">
 			
-			<form method="POST">
-				E-mail:<br/><br/>
-				<input type="email" name="email" /><br/><br/>
-				senha:<br/><br/>
-				<input type="password" name="senha" /><br/><br/>
-				<input type="submit" value="Entrar" />
-			</form>
-			<hr>
-			<form method="POST" >
-				<div class="form-row">
-					<div class="col">
-						<div class="form-group">
-            				<input id="nome" type="text" name="nome"  class="form-control" placeholder="Nome Completo:"/>
-        				</div>
-					</div>
-					<div class="col">
-						<div class="form-group">
-            				<input id="email" type="email" name="email"  class="form-control" placeholder="E-mail:"/>
-        				</div>
-					</div>
-					<div class="col">
-						<div class="form-group">
-            				<input id="senha" type="password" name="senha"  class="form-control" placeholder="Senha:"/>
-        				</div>
-					</div>
-				</div>
-				<div class="form-row">
-					<div class="col">
-						<div class="form-group">
-            				<input class="btn btn-primary" type="submit" value="Entrar" />
-        				</div>
-					</div>
-				</div>
-				
-			</form>
 			
 		</div>
 		
