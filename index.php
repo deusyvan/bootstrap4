bcc01cba4f38ee9d6f53bd68863363f7e96f995a
deusyvan <deusyvan@gmail.com>
2018-06-16 10:42:43 -0300

    Desativando um dos itens

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     			<ul class="list-group">
     				<li class="list-group-item active">Item 1</li>
     				<li class="list-group-item">Item 2</li>
-    				<li class="list-group-item">Item 3</li>
+    				<li class="list-group-item disabled">Item 3</li>
     			</ul>
 			</div>
 		</div>
