657d9889315408947f857acea6264dd10c41b33f
deusyvan <deusyvan@gmail.com>
2018-06-16 15:35:59 -0300

    Coloca o texto automaticamente onde puder aparecer

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<body>
 		<div class="container">
 			
-			<button class="btn btn-success" data-toggle="tooltip" data-placement="bottom"
+			<button class="btn btn-success" data-toggle="tooltip" 
 			     title="Ao clicar, irá salvar os dados do usuário">Salvar</button>
 			
 		</div>
