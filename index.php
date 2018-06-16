93d0e4cbaec4055dfde101d54009a4da7d9670fd
deusyvan <deusyvan@gmail.com>
2018-06-16 08:55:57 -0300

    Começando estudo de menu Dropdown

--- index.php
+++ index.php
@@ -15,63 +15,7 @@
 	<body>
 		<div class="container">
 			
-			<div id="accordion">
-				<div class="card">
-					<div class="card-header">
-						<button class="btn btn-link" data-toggle="collapse" 
-							data-target="#c1" aria-controls="c1">Título do CARD 1</button>
-					</div>
-					
-					<div class="collapse show" id="c1" data-parent="#accordion">
-    					<div class="card-body">
-    						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at ultrices velit.
-    						 Morbi ut pulvinar urna, non condimentum libero. Nam quis ante neque. 
-    						 In massa massa, tincidunt id sollicitudin eget, dictum in justo. 
-    						 Curabitur laoreet laoreet vehicula. Donec tincidunt, nunc quis aliquam faucibus, 
-    						 mi nisi posuere orci, id porta ex dolor in mauris. Pellentesque habitant morbi 
-    						 tristique senectus et netus et malesuada fames ac turpis egestas. Duis dolor 
-    						 ante, porttitor quis lorem eget, tincidunt ultrices est. Aliquam scelerisque felis 
-    						 ut diam posuere pharetra eu vitae ex. Nam nec sapien eu erat viverra placerat eu 
-    						 quis ex. Suspendisse urna velit, lacinia eu aliquet non, eleifend at leo. Mauris 
-    						 accumsan nulla sollicitudin elit volutpat, posuere auctor velit imperdiet. 
-    						 In tincidunt eleifend mi, a sollicitudin sapien mollis ac. Etiam ut rhoncus ligula,
-    						  vitae pretium nulla.
-    					</div>
-					</div>
-				</div>
-				
-				<div class="card">
-					<div class="card-header">
-						<button class="btn btn-link" data-toggle="collapse" 
-							data-target="#c2" aria-controls="c2">Título do CARD 2</button>
-					</div>
-					
-					<div class="collapse" id="c2" data-parent="#accordion">
-    					<div class="card-body">
-    						Sed tincidunt suscipit ante vitae consectetur. Aenean auctor ante velit, a laoreet massa rutrum sed. 
-    						Mauris id aliquet lectus. Ut at mauris bibendum urna malesuada sagittis. Etiam porta blandit elit in interdum.
-    						Nunc facilisis faucibus purus, a ornare neque. Vestibulum ante ipsum primis in faucibus orci luctus et 
-    						ultrices posuere cubilia Curae; Ut nisl magna.
-    					</div>
-					</div>
-				</div>
-				
-				<div class="card">
-					<div class="card-header">
-						<button class="btn btn-link" data-toggle="collapse" 
-							data-target="#c3" aria-controls="c3">Título do CARD 3</button>
-					</div>
-					
-					<div class="collapse" id="c3" data-parent="#accordion">
-    					<div class="card-body">
-    						Ut ac vehicula arcu. Nulla lorem diam, scelerisque in urna a, malesuada gravida justo. Mauris ac cursus est. 
-    						Sed congue magna non lobortis viverra. 
-    					</div>
-					</div>
-				</div>
-				
-				
-			</div>
+			
 			
 		</div>
 		
