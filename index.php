cd604eed952335b14521a60c644c0dcc81be7982
deusyvan <deusyvan@gmail.com>
2018-06-16 08:48:03 -0300

    Colocando texto maior e retirando a visualização ativa

--- index.php
+++ index.php
@@ -22,9 +22,20 @@
 							data-target="#c1" aria-controls="c1">Título do CARD 1</button>
 					</div>
 					
-					<div class="collapse show" id="c1" data-parent="#accordion">
+					<div class="collapse" id="c1" data-parent="#accordion">
     					<div class="card-body">
-    						Texto sobre o CARD
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at ultrices velit.
+    						 Morbi ut pulvinar urna, non condimentum libero. Nam quis ante neque. 
+    						 In massa massa, tincidunt id sollicitudin eget, dictum in justo. 
+    						 Curabitur laoreet laoreet vehicula. Donec tincidunt, nunc quis aliquam faucibus, 
+    						 mi nisi posuere orci, id porta ex dolor in mauris. Pellentesque habitant morbi 
+    						 tristique senectus et netus et malesuada fames ac turpis egestas. Duis dolor 
+    						 ante, porttitor quis lorem eget, tincidunt ultrices est. Aliquam scelerisque felis 
+    						 ut diam posuere pharetra eu vitae ex. Nam nec sapien eu erat viverra placerat eu 
+    						 quis ex. Suspendisse urna velit, lacinia eu aliquet non, eleifend at leo. Mauris 
+    						 accumsan nulla sollicitudin elit volutpat, posuere auctor velit imperdiet. 
+    						 In tincidunt eleifend mi, a sollicitudin sapien mollis ac. Etiam ut rhoncus ligula,
+    						  vitae pretium nulla.
     					</div>
 					</div>
 				</div>
