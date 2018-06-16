dce33e3f4b3148bc490427c3cd13bc0b4d7b022b
deusyvan <deusyvan@gmail.com>
2018-06-16 10:46:45 -0300

    Mostrando que o link executa uma ação

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 		<div class="container">
 			<div style="width: 600px; margin: 20px;">
     			<div class="list-group">
-    				<a class="list-group-item" href="#">Link para o site 1</a>
-    				<a class="list-group-item" href="#">Link para o site 2</a>
-    				<a class="list-group-item" href="#">Link para o site 3</a>
+    				<a class="list-group-item list-group-item-action" href="#">Link para o site 1</a>
+    				<a class="list-group-item list-group-item-action" href="#">Link para o site 2</a>
+    				<a class="list-group-item list-group-item-action" href="#">Link para o site 3</a>
     			</div>
 			</div>
 		</div>
