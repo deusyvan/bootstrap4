b0d97388a74cb659dbabff878c83114c81ea9b85
deusyvan <deusyvan@gmail.com>
2018-06-16 15:14:32 -0300

    Reinicando estudo com paginação

--- index.php
+++ index.php
@@ -15,26 +15,7 @@
 	<body>
 		<div class="container">
 			
-			<nav class="navbar navbar-expand-lg navbar-dark" 
-				style="background: url(assets/images/me.jpg) no-repeat;">
-				<a class="navbar-brand" href="#" >DFSWEb.com</a>
-				<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="navbar-collapse collapse" id="navbarMenu">
-				
-					<div class="navbar-nav">
-						<a href="#" class="nav-item nav-link active">Link 1</a>
-						<a href="#" class="nav-item nav-link ">Link 2</a>
-						<a href="#" class="nav-item nav-link ">Link 3</a>
-						<a href="#" class="nav-item nav-link ">Link 4</a>
-					</div>
-				
-				</div>
-				
-				<span class="navbar-text">Algum texto qualquer...</span>
-				
-			</nav>
+			
 			
 		</div>
 		
