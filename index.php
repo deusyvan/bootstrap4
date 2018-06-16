9da424a4504a4a923e00129dd0b0e8de469bd70b
deusyvan <deusyvan@gmail.com>
2018-06-16 10:39:14 -0300

    Uma lista com bootstrap

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
 	<body>
 		<div class="container">
 			
-			<ul>
-				<li>Item 1</li>
-				<li>Item 2</li>
-				<li>Item 3</li>
+			<ul class="list-group">
+				<li class="list-group-item">Item 1</li>
+				<li class="list-group-item">Item 2</li>
+				<li class="list-group-item">Item 3</li>
 			</ul>
 			
 		</div>
