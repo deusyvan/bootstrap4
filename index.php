85645d11cc708675d84ea27e920567011772504c
deusyvan <deusyvan@gmail.com>
2018-06-16 15:19:12 -0300

    Estrutura da paginação

--- index.php
+++ index.php
@@ -15,7 +15,13 @@
 	<body>
 		<div class="container">
 			
-			
+			<ul class="pagination">
+				<li class="page-item"><a href="#" class="page-link">1</a></li>
+				<li class="page-item"><a href="#" class="page-link">2</a></li>
+				<li class="page-item"><a href="#" class="page-link">3</a></li>
+				<li class="page-item"><a href="#" class="page-link">4</a></li>
+				<li class="page-item"><a href="#" class="page-link">5</a></li>
+			</ul>
 			
 		</div>
 		
